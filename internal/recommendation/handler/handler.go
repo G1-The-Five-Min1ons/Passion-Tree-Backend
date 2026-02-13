c8a7aed85372492403f87222e6380eb551c19464
OHMll <napadechkaewnok@gmail.com>
2026-02-13 23:22:08 +0700

    feature(recommend): ร่างโครงสร้างเส้นและ logic recommend

---
+++ internal/recommendation/handler/handler.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+	"log/slog"
+	"passiontree/internal/pkg/apperror"
+	"passiontree/internal/pkg/storage"
+	"passiontree/internal/recommendation/service"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Handler struct {
+	recreflectSvc service.ServiceRecommendation
+	logger        *slog.Logger
+	storage       *storage.BlobService
+}
+
+func NewHandler(svc service.Service, logger *slog.Logger, storage *storage.BlobService) *Handler {
+	return &Handler{
+		recreflectSvc: svc,
+		logger:        logger,
+		storage:       storage,
+	}
+}
+
+func (h *Handler) handleError(c *fiber.Ctx, err error) error {
+	ctx := c.UserContext()
+
+	// Common Attributes
+	logAttrs := []any{
+		"method", c.Method(),
+		"path", c.Path(),
+		"ip", c.IP(),
+		"user_agent", c.Get("User-Agent"),
+		"request_id", c.GetRespHeader("X-Request-ID"), // Request ID Middleware
+	}
+
+	// Business/Handled Error)
+	if appErr, ok := err.(*apperror.AppError); ok {
+		if appErr.Log != nil {
+			h.logger.WarnContext(ctx, "application handled error",
+				append(logAttrs,
+					"code", appErr.Code,
+					"message", appErr.Message,
+					"cause", appErr.Log,
+				)...,
+			)
+		}
+
+		return c.Status(appErr.Code).JSON(fiber.Map{
+			"success": false,
+			"error":   appErr.Message,
+		})
+	}
+
+	// System Error (Unhandled Error)
+	h.logger.ErrorContext(ctx, "unhandled system error",
+		append(logAttrs, "error", err.Error())...,
+	)
+
+	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+		"success": false,
+		"error":   "internal server error",
+	})
+}
