e533043ea7583b810dffdb637b4c5948915d26aa
farlos <thiraphat.pant@gmail.com>
2026-03-01 14:25:54 +0700

    refactor(Middleware): Change ratelimit for mock data

--- internal/pkg/middleware/ratelimit.go
+++ internal/pkg/middleware/ratelimit.go
@@ -8,18 +8,18 @@ import (
 )
 
 func RateLimitMiddleware() fiber.Handler {
-    return limiter.New(limiter.Config{
-        Max:        10, 
-        Expiration: 5 * time.Minute,
-        KeyGenerator: func(c *fiber.Ctx) string {
-            // lock with IP for prevent Bot spam (Dos)
-            return "limit:ip:" + c.IP()
-        },
-        LimitReached: func(c *fiber.Ctx) error {
-            return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
-                "success": false,
-                "error":   "Too many requests from this IP. Please wait 5 minutes.",
-            })
-        },
-    })
+	return limiter.New(limiter.Config{
+		Max:        10,
+		Expiration: 5 * time.Minute,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			// lock with IP for prevent Bot spam (Dos)
+			return "limit:ip:" + c.IP()
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+				"success": false,
+				"error":   "Too many requests from this IP. Please wait 5 minutes.",
+			})
+		},
+	})
 }
